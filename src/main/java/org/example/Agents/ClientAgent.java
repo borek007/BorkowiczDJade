e33d65261ffd17639b49b6879ef3251a3e2aba4a
Dominik Borkowicz <dborkow7@gmail.com>
2024-04-07 15:01:46 +0200

    ClientAgent logic implemented

--- src/main/java/org/example/Agents/ClientAgent.java
+++ src/main/java/org/example/Agents/ClientAgent.java
@@ -4,115 +4,204 @@ import jade.core.AID;
 import jade.core.Agent;
 import jade.core.behaviours.Behaviour;
 import jade.core.behaviours.SequentialBehaviour;
-import jade.domain.DFService;
-import jade.domain.FIPAAgentManagement.DFAgentDescription;
-import jade.domain.FIPAAgentManagement.ServiceDescription;
-import jade.domain.FIPAException;
 import jade.lang.acl.ACLMessage;
+import jade.lang.acl.MessageTemplate;
+import jade.util.leap.Iterator;
 
+import java.io.Serializable;
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 
-public class ClientAgent extends Agent {
+public class ClientAgent extends Agent implements Serializable {
     private String areaCode;
+    private String book;
+
+    public ClientAgent(String book, String areaCode) {
+        this.book = book;
+        this.areaCode = areaCode;
+    }
+
 
     protected void setup() {
-        System.out.println("Client-agent "+getAID().getName()+" is ready.");
+        System.out.println("Client-agent " + getAID().getName() + " is ready.");
 
         Object[] args = getArguments();
         if (args != null && args.length > 0) {
             areaCode = (String) args[0];
-            System.out.println("Target area code is "+areaCode);
+            System.out.println("Target area code is " + areaCode);
 
-            SequentialBehaviour setupBehaviour = new SequentialBehaviour();
-            setupBehaviour.addSubBehaviour(new FindBookstoresBehaviour(this, areaCode));
-            setupBehaviour.addSubBehaviour(new RequestBookBehaviour(this));
-            setupBehaviour.addSubBehaviour(new SelectBookstoreBehaviour(this));
-            addBehaviour(setupBehaviour);
-        }
-        else {
+            SequentialBehaviour buyBookBehaviour = new SequentialBehaviour();
+            buyBookBehaviour.addSubBehaviour(new FindBookstoresBehaviour(this));
+            buyBookBehaviour.addSubBehaviour(new RequestBookBehaviour(this));
+            buyBookBehaviour.addSubBehaviour(new FinishTransactionBehaviour(this));
+            addBehaviour(buyBookBehaviour);
+        } else {
             System.out.println("No target area code specified");
             doDelete();
         }
     }
 
     protected void takeDown() {
-        System.out.println("Client-agent "+getAID().getName()+" terminating.");
+        System.out.println("Client-agent " + getAID().getName() + " terminating.");
     }
+
     private class FindBookstoresBehaviour extends Behaviour {
-        private String areaCode;
+
         private List<AID> sellerAgents;
 
-        public FindBookstoresBehaviour(Agent a, String areaCode) {
+        public FindBookstoresBehaviour(ClientAgent a) {
             super(a);
-            this.areaCode = areaCode;
+
             this.sellerAgents = new ArrayList<>();
         }
 
         public void action() {
-            DFAgentDescription template = new DFAgentDescription();
-            ServiceDescription sd = new ServiceDescription();
-            sd.setType(areaCode);
-            template.addServices(sd);
-            try {
-                DFAgentDescription[] result = DFService.search(myAgent, template);
-                for (int i = 0; i < result.length; ++i) {
-                    sellerAgents.add(result[i].getName());
-                }
+
+            ACLMessage msg = new ACLMessage(ACLMessage.REQUEST);
+            msg.setContent(areaCode);
+            msg.addReceiver(DirectorFacilitatorAgent.getInstance().getAID());
+            myAgent.send(msg);
+            ACLMessage reply = myAgent.blockingReceive();
+            String content = reply.getContent();
+            String[] AIDS = content.split(",");
+            for (String aid : AIDS) {
+                sellerAgents.add(new AID(aid, AID.ISLOCALNAME));
             }
-            catch (FIPAException fe) {
-                fe.printStackTrace();
-            }
+            getDataStore().put("sellerAgents", sellerAgents);
+
         }
 
         public boolean done() {
             return !sellerAgents.isEmpty();
         }
 
-        public List<AID> getSellerAgents() {
-            return sellerAgents;
-        }
     }
-    private class RequestBookBehaviour extends Behaviour {
-        private AID sellerAgent;
 
-        public RequestBookBehaviour(Agent a, AID sellerAgent) {
+
+    private class RequestBookBehaviour extends Behaviour {
+        ACLMessage msg;
+        List<AID> sellerAgents;
+        Boolean done = false;
+
+        public RequestBookBehaviour(Agent a) {
             super(a);
-            this.sellerAgent = sellerAgent;
+
+        }
+
+        private void setup() {
+            sellerAgents = (List<AID>) getDataStore().get("sellerAgents");
+            msg = new ACLMessage(ACLMessage.REQUEST);
+            msg.setContent("Requesting book: " + book);
         }
 
         public void action() {
-            ACLMessage msg = new ACLMessage(ACLMessage.REQUEST);
-            msg.addReceiver(sellerAgent);
-            msg.setContent("Requesting book");
+            if (sellerAgents == null || msg == null)
+                setup();
+
+
+            sendRequest();
+            getRespones();
+        }
+
+        private void getRespones() {
+            ArrayList<Offer> responses = new ArrayList<>();
+
+            // Receive the replies
+            for (Iterator it = msg.getAllReplyTo(); it.hasNext(); ) {
+                // in case of problems with getAllReplyTo switch to MessageTemplate.MatchInReplyTo
+                ACLMessage reply = (ACLMessage) it.next();
+                if (reply != null) {
+                    // Handle the reply from the SellerAgent
+                    // For now, we just print out a message
+                    System.out.println("Received a reply from " + reply.getSender().getName() + " with content: " + reply.getContent());
+                    responses.add(new Offer(reply.getSender().getName(), book, Double.parseDouble(reply.getContent())));
+
+                }
+
+
+            }
+            getDataStore().put("responses", responses);
+            done = true;
+
+        }
+
+        private void sendRequest() {
+
+
+            for (AID sellerAgent : sellerAgents) {
+                msg.addReceiver(sellerAgent);
+            }
+
+
             myAgent.send(msg);
         }
+
 
         public boolean done() {
             // This behaviour is done when it has sent the request
             return true;
         }
     }
-    public class SelectBookstoreBehaviour extends Behaviour {
-        private List<AID> sellerAgents;
 
-        public SelectBookstoreBehaviour(Agent a, List<AID> sellerAgents) {
+
+    private class FinishTransactionBehaviour extends Behaviour {
+        private boolean done = false;
+
+        public FinishTransactionBehaviour(Agent a) {
             super(a);
-            this.sellerAgents = sellerAgents;
         }
 
         public void action() {
-            // For now, we just select the first sellerAgent from the list
-            // TODO: Add logic to select a bookstore based on your criteria
-            if (!sellerAgents.isEmpty()) {
-                AID selectedSellerAgent = sellerAgents.get(0);
-                System.out.println("Selected bookstore: " + selectedSellerAgent.getName());
+            List<Offer> responses = (ArrayList<Offer>) getDataStore().get("responses");
+            if (responses == null) {
+                done = false;
+                return;
             }
+            Offer bestOffer = responses.stream().min(Comparator.comparingDouble(Offer::getPrice)).get();
+            System.out.println("Best offer: " + bestOffer.getSeller() + " with price: " + bestOffer.getPrice());
+
+            MessageTemplate mt = MessageTemplate.MatchPerformative(ACLMessage.REQUEST);
+            ACLMessage msg = new ACLMessage(ACLMessage.REQUEST);
+            msg.setContent("Buying book: " + book + "for price: " + bestOffer.getPrice());
+            msg.addReceiver(new AID(bestOffer.getSeller(), AID.ISLOCALNAME));
+            myAgent.send(msg);
+            ACLMessage reply = myAgent.blockingReceive(mt);
+            System.out.println("Received a reply from " + reply.getSender().getName() + " with content: " + reply.getContent());
+            done = true;
+
+
         }
 
+        @Override
         public boolean done() {
-            // This behaviour is done when it has selected a bookstore
-            return !sellerAgents.isEmpty();
+            return done;
         }
     }
+
+    public class Offer {
+        private String seller;
+        private String book;
+        private double price;
+
+        public Offer(String seller, String book, double price) {
+            this.seller = seller;
+            this.book = book;
+            this.price = price;
+        }
+
+        public String getSeller() {
+            return seller;
+        }
+
+        public String getBook() {
+            return book;
+        }
+
+        public double getPrice() {
+            return price;
+        }
+    }
+
+
 }
