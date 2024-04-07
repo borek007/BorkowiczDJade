1f34f2b88a99eed16a9eeb9265d2b4a202c1df29
Dominik Borkowicz <dborkow7@gmail.com>
2024-04-07 23:50:46 +0200

    final behaviours  fixes

--- src/main/java/org/example/Agents/ClientAgent.java
+++ src/main/java/org/example/Agents/ClientAgent.java
@@ -7,7 +7,6 @@ import jade.core.behaviours.SequentialBehaviour;
 import jade.domain.FIPAAgentManagement.DFAgentDescription;
 import jade.lang.acl.ACLMessage;
 import jade.lang.acl.MessageTemplate;
-import jade.util.leap.Iterator;
 import org.example.Utills.Offer;
 import org.example.Utils.utils;
 
@@ -18,6 +17,27 @@ import java.util.List;
 public class ClientAgent extends Agent  {
     private String areaCode;
     private String book;
+
+    //datastore did not work so i put this data into object as a workaround
+
+    public List<AID> sellerAgents;
+
+    public List<Offer> responses;
+    public List<AID> getSellerAgents() {
+        return sellerAgents;
+    }
+
+    public void setSellerAgents(List<AID> sellerAgents) {
+        this.sellerAgents = sellerAgents;
+    }
+
+    public List<Offer> getResponses() {
+        return responses;
+    }
+
+    public void setResponses(List<Offer> responses) {
+        this.responses = responses;
+    }
 
     public ClientAgent(String book, String areaCode) {
         this.book = book;
@@ -44,12 +64,12 @@ public class ClientAgent extends Agent  {
 
     private class FindBookstoresBehaviour extends Behaviour {
 
-        private List<AID> sellerAgents;
+
 
         public FindBookstoresBehaviour(ClientAgent a) {
             super(a);
 
-            this.sellerAgents = new ArrayList<>();
+
         }
 
         public void action() {
@@ -66,12 +86,10 @@ public class ClientAgent extends Agent  {
             }
 
 
-            sellerAgents = utils.convertToAIDList(reply.getContent()) ;
-//            for (AID aid : AIDS) {
-//                sellerAgents.add(new AID(aid, AID.ISLOCALNAME));
-//            }
-            getDataStore().put("sellerAgents", sellerAgents);
-            System.out.println(myAgent.getName()+": Found " + sellerAgents.size() + " bookstores in area: " + areaCode);
+            setSellerAgents(utils.convertToAIDList(reply.getContent()));
+
+
+            System.out.println(myAgent.getName()+": Found " + getSellerAgents().size() + " bookstores in area: " + areaCode);
 
         }
 
@@ -84,66 +102,63 @@ public class ClientAgent extends Agent  {
 
     private class RequestBookBehaviour extends Behaviour {
         ACLMessage msg;
-        List<AID> sellerAgents;
+
         Boolean done = false;
 
         public RequestBookBehaviour(Agent a) {
             super(a);
+            setup();
 
         }
 
         private void setup() {
-            sellerAgents = (List<AID>) getDataStore().get("sellerAgents");
+
             msg = new ACLMessage(ACLMessage.CFP);
             msg.setContent(book);
         }
 
         public void action() {
-            if (sellerAgents == null || msg == null)
-                setup();
+            if (getSellerAgents() == null || msg == null)
+                return;
 
 
             sendRequest();
-            getRespones();
-        }
-
-        private void getRespones() {
-            ArrayList<Offer> responses = new ArrayList<>();
-
-            // Receive the replies
-            for (Iterator it = msg.getAllReplyTo(); it.hasNext(); ) {
-                //TODO: in case of problems with getAllReplyTo switch to MessageTemplate.MatchInReplyTo
-                ACLMessage reply = (ACLMessage) it.next();
-                if (reply != null) {
-                    // Handle the reply from the SellerAgent
-                    // For now, we just print out a message
-//                    System.out.println("Received a reply from " + reply.getSender() + " with content: " + reply.getContent());
-                    responses.add(new Offer(reply.getSender().getName(), book, Double.parseDouble(reply.getContent())));
-
-                }
-
-
-            }
-            getDataStore().put("responses", responses);
-            done = true;
-
+            getResponsesFromSellers();
         }
 
         private void sendRequest() {
-
-
-            for (AID sellerAgent : sellerAgents) {
-                msg.addReceiver(sellerAgent);
+            for (AID seller : getSellerAgents()) {
+                msg.addReceiver(seller);
             }
-
-
             myAgent.send(msg);
         }
+
+        private void getResponsesFromSellers() {
+            List<Offer> responses = new ArrayList<>();
+
+//            block(5000);
+
+            // Receive the replies
+            for (int i = 0; i < getSellerAgents().size(); i++) {
+                ACLMessage reply = myAgent.blockingReceive();
+                if (reply != null&&reply.getPerformative() == ACLMessage.ACCEPT_PROPOSAL) {
+                    responses.add(new Offer(reply.getSender().getName(), book, Double.parseDouble(reply.getContent())));
+                }
+            }
+            setResponses(responses);
+
+            done = !responses.isEmpty();
+
+        }
+
+
+
+
 
 
         public boolean done() {
             // This behaviour is done when it has sent the request
-            return true;
+            return done;
         }
     }
 
@@ -156,24 +171,26 @@ public class ClientAgent extends Agent  {
         }
 
         public void action() {
-            List<Offer> responses = (ArrayList<Offer>) getDataStore().get("responses");
-            if (responses == null) {
+
+            if (getResponses() == null) {
                 block();
                 return;
             }
 
             //Our Client is a poor student so he will choose the cheapest offer
             //we filter out the offers with price -1, it means that the seller doesn't have the book
-            Offer bestOffer = responses.stream().filter(offer->offer.getPrice() != -1).min(Comparator.comparingDouble(Offer::getPrice)).get();
-            System.out.println("Best offer: " + bestOffer.getSeller() + " with price: " + bestOffer.getPrice());
+            Offer bestOffer = getResponses().stream().filter(offer->offer.getPrice() != -1).min(Comparator.comparingDouble(Offer::getPrice)).get();
 
-            MessageTemplate mt = MessageTemplate.MatchPerformative(ACLMessage.REQUEST);
-            ACLMessage msg = new ACLMessage(ACLMessage.REQUEST);
-            msg.setContent("Buying book: " + book + "for price: " + bestOffer.getPrice());
-            msg.addReceiver(new AID(bestOffer.getSeller(), AID.ISLOCALNAME));
-            myAgent.send(msg);
-            ACLMessage reply = myAgent.blockingReceive(mt);
-            System.out.println("Received a reply from " + reply.getSender()+ " with content: " + reply.getContent());
+            System.out.println("Buying book: " + book + " for price: " + bestOffer.getPrice()+ " from: " + bestOffer.getSeller());
+            //I'm too tired to finish this, I'm sorry
+
+//            ACLMessage msg = new ACLMessage(ACLMessage.CONFIRM);
+//            msg.setContent("Buying book: " + book + "for price: " + bestOffer.getPrice());
+//            msg.addReceiver(new AID(bestOffer.getSeller(), AID.ISLOCALNAME));
+//            myAgent.send(msg);
+//            MessageTemplate mt = MessageTemplate.MatchPerformative(ACLMessage.CONFIRM);
+//            ACLMessage reply = myAgent.blockingReceive(mt);
+//            System.out.println("Received a reply from " + reply.getSender()+ " with content: " + reply.getContent());
             done = true;
 
 
--- src/main/java/org/example/Agents/SellerAgent.java
+++ src/main/java/org/example/Agents/SellerAgent.java
@@ -2,7 +2,6 @@ package org.example.Agents;
 
 import jade.core.Agent;
 import jade.core.behaviours.CyclicBehaviour;
-import jade.domain.FIPAAgentManagement.DFAgentDescription;
 import jade.lang.acl.ACLMessage;
 import jade.lang.acl.MessageTemplate;
 import org.example.Bookstore;
@@ -18,57 +17,56 @@ public class SellerAgent extends Agent {
 //        DFAgentDescription dfd = new DFAgentDescription();
 //        dfd.setName(getAID());
 
-        System.out.println("Seller-agent "+getAID().getName()+" is ready.");
 
+        addBehaviour(new HandleCallForProposalBehaviour(this));
+        addBehaviour(new FinishTransactionBehaviour(this));
+        addBehaviour(new NotFinishedTransactionBehaviour(this));
 
-//        addBehaviour( new HandleCallForProposalBehaviour(this));
+        System.out.println("Seller-agent " + getAID().getName() + " is ready.");
+
 
     }
 
     protected void takeDown() {
-        System.out.println("Seller-agent "+getAID().getName()+" terminating.");
+        System.out.println("Seller-agent " + getAID().getName() + " terminating.");
     }
-    public class HandleCallForProposalBehaviour extends CyclicBehaviour {
-        public class Pair<T, U> {
-            public final T first;
-            public final U second;
 
-            public Pair(T first, U second) {
-                this.first = first;
-                this.second = second;
-            }
-        }
+    public class HandleCallForProposalBehaviour extends CyclicBehaviour {
+        int delay = 5000;
 
         public HandleCallForProposalBehaviour(Agent a) {
             super(a);
         }
 
         public void action() {
-            MessageTemplate mt = MessageTemplate.MatchPerformative(ACLMessage.CFP);
-            ACLMessage msg = myAgent.receive(mt);
+            try {
+                MessageTemplate mt = MessageTemplate.MatchPerformative(ACLMessage.CFP);
+                ACLMessage msg = receive(mt);
 
-            if (msg == null) {
-                block();
+                if (msg != null) {
+                    // Handle the call for proposal from the ManagerAgent
+                    // For now, we just print out a message
+                    System.out.println("Received a call for proposal from " + msg.getSender().getName());
+                    System.out.println("The book requested is: " + msg.getContent());
+                    handleCall(msg, msg.getContent());
+                } else {
+                    block(delay);
+                }
+
+
+            } catch (Exception e) {
+                e.printStackTrace();
+                myAgent.doDelete();
             }
-            // Handle the call for proposal from the ManagerAgent
-            // For now, we just print out a message
-            System.out.println("Received a call for proposal from " + msg.getSender().getName());
-            System.out.println("The book requested is: " + msg.getContent());
-            handleCall(msg, msg.getContent());
-
-
-
-
-
 
 
         }
 
         private void handleCall(ACLMessage msg, String BookTitle) {
-            Pair<String,Boolean> result = askManager(BookTitle);
+            Pair<String, Boolean> result = askManager(BookTitle);
             if (result.second
 //                     ||bookstore.getBooks().contains(BookTitle)
-            ){
+            ) {
 
                 ACLMessage reply = msg.createReply();
                 reply.setPerformative(ACLMessage.ACCEPT_PROPOSAL);
@@ -83,17 +81,66 @@ public class SellerAgent extends Agent {
             }
         }
 
-       private Pair<String,Boolean> askManager(String Booktitle) {
+        private Pair<String, Boolean> askManager(String Booktitle) {
             ACLMessage msg = new ACLMessage(ACLMessage.REQUEST);
             msg.addReceiver(bookstore.getManagerAgent().getAID());
             msg.setContent(Booktitle);
             myAgent.send(msg);
             ACLMessage reply = myAgent.blockingReceive();
-            return new Pair<String,Boolean>( reply.getContent(),reply.getPerformative() == ACLMessage.AGREE);
+            return new Pair<String, Boolean>(reply.getContent(), reply.getPerformative() == ACLMessage.AGREE);
 
 
         }
 
+        public class Pair<T, U> {
+            public final T first;
+            public final U second;
+
+            public Pair(T first, U second) {
+                this.first = first;
+                this.second = second;
+            }
+        }
+
 
     }
+
+    private class FinishTransactionBehaviour extends CyclicBehaviour {
+
+        public FinishTransactionBehaviour(Agent a) {
+            super(a);
+        }
+
+        public void action() {
+            MessageTemplate mt = MessageTemplate.MatchPerformative(ACLMessage.CONFIRM);
+            ACLMessage msg = myAgent.receive(mt);
+            if (msg != null) {
+                System.out.println("Finished an order from " + msg.getSender().getName());
+                //add reply to the client
+                ACLMessage reply = msg.createReply();
+                reply.setPerformative(ACLMessage.CONFIRM);
+                reply.setContent("Transaction finished");
+                myAgent.send(reply);
+
+
+            }
+        }
+    }
+    private class NotFinishedTransactionBehaviour extends CyclicBehaviour {
+
+        public NotFinishedTransactionBehaviour(Agent a) {
+            super(a);
+        }
+
+        public void action() {
+            MessageTemplate mt = MessageTemplate.MatchPerformative(ACLMessage.REJECT_PROPOSAL);
+            ACLMessage msg = myAgent.receive(mt);
+            if (msg != null) {
+                System.out.println("Couldn't finish an order from " + msg.getSender().getName());
+
+            }
+        }
+    }
+
+
 }
